f633503e0245c397f8c239fbf92dea71e88623f9
github-praveen <45990117+github-praveen@users.noreply.github.com>
2019-01-07 15:49:45 +0530

    Add files via upload

---
+++ CommonPackageBAL/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OtpNet;
+
+namespace CommonPackageBAL
+{
+    public class Class1
+    {
+        Byte[] bytes = OtpNet.Base32Encoding.ToBytes("35325");
+        //var totp = new OtpNet.Totp(bytes);
+    }
+}
